b43b92ff752f798bc0c518e1f09205797ef90b75
szymon.krzysztofik <szymon.krzysztofik@sdqac.rd.tp.pl>
2017-03-20 12:04:40 +0100

    first commit

---
+++ appsite/blog/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from datetime import datetime
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=datetime.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = datetime.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
